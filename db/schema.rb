e47d5485fb62841aa7ec0aeb1d186859475c94cc
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-23 16:25:18 +0100

    model farm creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_151348) do
+ActiveRecord::Schema.define(version: 2020_11_23_152206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,21 @@ ActiveRecord::Schema.define(version: 2020_11_23_151348) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "farms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.string "photos"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "sells"
+    t.string "opening_time"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_farms_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_11_23_151348) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "farms", "users"
 end
