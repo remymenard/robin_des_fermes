c4bdc7f417b4fcb907d8c6545ccfb34d3061a196
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-23 16:01:49 +0100

    front of the home page footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Farm.destroy_all
+
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint_leaves = Ingredient.create!(name: "mint leaves")
+
