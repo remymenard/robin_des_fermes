5095219b18e9a23666a78715e0ee93db79868a53
Remy Menard <remymenard.work@gmail.com>
2020-11-23 11:51:37 +0100

    remove useless payment route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /fr/ do
     devise_for :users
     root to: 'pages#home'
-    get '/payment', to: 'pages#payment'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
