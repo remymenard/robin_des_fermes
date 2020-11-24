91feac09458381ba9d193de36c76f8ba444779d2
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-24 14:31:38 +0100

    button to search by category which is functional

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,11 +1,15 @@
 class FarmsController < ApplicationController
   def index
     @farms = Farm.all
+    @categories = Category.all
+    if params[:query].present?
+      @farms = Farm.where_category_name(params[:query])
+    end
   end
 
   private
 
   def article_params
-    params.require(:farm).permit(:name, :description, :photo, :adress, :sells, :opening_time, :labels)
+    params.require(:farm).permit(:name, :description, :photo, :adress, :sells, :opening_time, :labels, :category_id)
   end
 end
