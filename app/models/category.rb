88efd0d51505dfff6f58a968550c45ff9d7cf067
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-24 19:34:17 +0100

    Move seed images into a fixtures folder

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :farm_categories
   has_many :farms, through: :farm_categories
 
+  has_one_attached :image
+
   CATEGORIES = ["Viande", "Laitier", "Poisson", "Fruit", "Boulangerie", "Oeuf", "Céréale", "Huile", "Vin", "Divers"]
   validates :name, inclusion: { in: CATEGORIES }
 end
