fe0d767dadd0e4af386f149b1319e3b028390e30
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-24 06:46:06 +0100

    implementation of farm seeds + addition of presence validation

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,3 +1,13 @@
 class Farm < ApplicationRecord
   belongs_to :user
+
+  has_many_attached :photos
+
+  LABELS = ['bio']
+
+  validates :name, presence: true
+  validates :labels, presence: true
+  validates :sells, presence: true
+  validates :address, presence: true
+  validates :opening_time, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,26 +10,35 @@ require "open-uri"
 User.destroy_all
 Farm.destroy_all
 
-user = User.create!(
+user1 = User.create!(
   email: "test@exemp.com",
-  password: "password")
+  password: "password",
+  name: "henry")
+user1.photo.attach(io: File.open('app/assets/images/seed/user1.png'), filename: 'seed-henry.png')
 
-henry = Farm.create!(name: "Famille Henry", user: user)
-henry.photos.attach(io: File.open('app/assets/images/seed/seed-meleze.png'), filename: 'seed-henry.png')
+user2 = User.create!(
+  email: "test@exemple.com",
+  password: "password",
+  name: "jonas")
+user2.photo.attach(io: File.open('app/assets/images/seed/user2.png'), filename: 'seed-henry.png')
 
 
-file2 = File.open('app/assets/images/seed/seed-henry.png')
-meleze = Farm.create!(name: "La ferme du Mélèze", user: user)
+henry = Farm.create!(name: "Famille Henry", user: user1, labels: 'bio', sells: 19, address: 'Nantes', opening_time: '8h-17h')
+henry.photos.attach(io: File.open('app/assets/images/seed/farm3.png'), filename: 'seed-henry.png')
+
+
+file2 = File.open('app/assets/images/seed/farm1.png')
+meleze = Farm.create!(name: "La ferme du Mélèze", user: user1, labels: 'bio', sells: 18, address: 'Pornichet', opening_time: '8h-17h')
 meleze.photos.attach(io: file2, filename: 'nes.png', content_type: 'image/png')
 
-file3 = URI.open('app/assets/images/seed/seed-henry.png')
-jonas = Farm.create!(name: "La Ferme de Jonas", user: user)
+file3 = URI.open('app/assets/images/seed/farm2.png')
+jonas = Farm.create!(name: "La Ferme de Jonas", user: user2, labels: 'bio', sells: 38, address: 'Clavan', opening_time: '8h-17h')
 jonas.photos.attach(io: file3, filename: 'nes.png', content_type: 'image/png')
 
-file4 = URI.open('app/assets/images/seed/seed-henry.png')
-cave = Farm.create!(name: "La Cave de l'Abbatiale", user: user)
+file4 = URI.open('app/assets/images/seed/farm3.png')
+cave = Farm.create!(name: "La Cave de l'Abbatiale", user: user2, labels: 'bio', sells: 28, address: 'Paris', opening_time: '8h-17h')
 cave.photos.attach(io: file4, filename: 'nes.png', content_type: 'image/png')
 
-file5 = URI.open('app/assets/images/seed/seed-henry.png')
-gallien = Farm.create!(name: "Le Domaine du Gallien", user: user)
+file5 = URI.open('app/assets/images/seed/farm1.png')
+gallien = Farm.create!(name: "Le Domaine du Gallien", user: user2, labels: 'bio', sells: 12, address: 'Toulouse', opening_time: '8h-17h')
 gallien.photos.attach(io: file5, filename: 'nes.png', content_type: 'image/png')
