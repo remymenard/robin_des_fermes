72e3394c3b60b04134d2b25b427090f7ce38ae2f
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-24 19:44:22 +0100

    Fix - seeds working with right paths for images

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :farm_categories
   has_many :farms, through: :farm_categories
 
-  has_one_attached :image
+  has_one_attached :photo
 
   CATEGORIES = ["Viande", "Laitier", "Poisson", "Fruit", "Boulangerie", "Oeuf", "Céréale", "Huile", "Vin", "Divers"]
   validates :name, inclusion: { in: CATEGORIES }
