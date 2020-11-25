bdb12155ae7c82a71df0646fa09c3c7bb2debc20
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-25 14:52:57 +0100

    creation of routes and product controller + link to go to a product show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /fr/ do
     devise_for :users
     root to: 'pages#home'
-    resources :farms, only: [:index, :show]
+
+    resources :farms, only: [:index, :show] do
+      resources :products, only: [:show]
+    end
   end
 
 
