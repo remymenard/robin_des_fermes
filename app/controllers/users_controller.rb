6569c3f97d5f6b55558a8090bbc15fc83c76d9d7
Remy Menard <remymenard.work@gmail.com>
2020-11-25 18:04:38 +0100

    add zip_code cookie storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :set_zip_code ]
+
   def set_zip_code
     if params["zip_code"]
       if params["zip_code"].match(/[1-9]\d{3}/)
         if current_user
           current_user.update(zip_code: params["zip_code"])
         else
-
+          puts "SET"
+          cookies.permanent[:zip_code] = params["zip_code"]
         end
       end
     end
   end
+
 end
