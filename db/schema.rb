a3767f06c8ac4eb508b526e6ec7d0b96a49c293b
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-26 15:52:37 +0100

    creation of the regions column in the farm table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_084432) do
+ActiveRecord::Schema.define(version: 2020_11_26_145107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_084432) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "labels", default: [], array: true
+    t.jsonb "regions", default: [], array: true
     t.index ["user_id"], name: "index_farms_on_user_id"
   end
 
