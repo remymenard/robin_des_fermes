a29a4abf4052f2fb68632572c7115eb8b3b7918e
Maelie Poussier <maelie.poussier@icloud.com>
2020-11-27 08:19:20 +0100

    addition of the withdrawal column which is a boolean to the farm table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_084432) do
+ActiveRecord::Schema.define(version: 2020_11_27_071656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_084432) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "labels", default: [], array: true
+    t.jsonb "regions", default: [], array: true
+    t.boolean "withdrawal"
     t.index ["user_id"], name: "index_farms_on_user_id"
   end
 
