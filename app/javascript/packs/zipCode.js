814ec3c93904ff8a41b868db6b9b5c9c8cefc4f3
Remy Menard <remymenard.work@gmail.com>
2020-11-30 09:14:13 +0100

    add login and zip_code path in html datasets

--- app/javascript/packs/zipCode.js
+++ app/javascript/packs/zipCode.js
@@ -17,13 +17,15 @@ $('#zip_code').on("click", async () => {
   }
 
   if(!$('#zip_code').data("signedin?")) {
-    baseModal.footer = `<a href="/users/sign_in">${I18n.t('zip_code.login')}</a>`
+    const loginPath = $('#zip_code').data("login_path")
+    baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
   }
   const { value: zip_code } = await Swal.fire(baseModal)
 
+  const setZipCodePath = $('#zip_code').data("set_zip_code_path")
   $.ajax({
     data: { authenticity_token: $('#zip_code').data("token"), zip_code:  zip_code},
-    url: "/set_zip_code/",
+    url: setZipCodePath,
     type: "POST"
   })
 
