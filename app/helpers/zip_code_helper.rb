37fe21f2e67e0cfd48f10fc87b4730723d7d8a7f
Remy Menard <remymenard.work@gmail.com>
2020-11-30 18:08:00 +0100

    add ZipCodeHelper and refacto JS zipCode pack

---
+++ app/helpers/zip_code_helper.rb
@@ -0,0 +1,21 @@
+module ZipCodeHelper
+
+  def get_zip_code_div
+    zip_code = get_zip_code_number
+    if zip_code
+      return "<p class='zip_code_number'>#{zip_code}</p>"
+    else
+      return "<p class='zip_code_number'>__________</p>"
+    end
+  end
+
+  def get_zip_code_number
+    if user_signed_in?
+      return current_user.zip_code if current_user.zip_code
+      return nil
+    else
+      return cookies[:zip_code] if cookies[:zip_code]
+      return nil
+    end
+  end
+end
--- app/javascript/packs/zipCode.js
+++ app/javascript/packs/zipCode.js
@@ -1,34 +1,50 @@
 import Swal from 'sweetalert2'
 
+document.addEventListener('turbolinks:load', () => {
 
-$('#zip_code').on("click", async () => {
-  let baseModal = {
-    title: I18n.t('zip_code.title'),
-    inputLabel: I18n.t('zip_code.description'),
-    input: 'number',
-    inputPlaceholder: '0000',
-    confirmButtonColor: '#007C50',
-    preConfirm: (zip_code) => {
-      if (!/^[1-9]\d{3}$/.test(zip_code)) {
-        Swal.showValidationMessage(I18n.t('zip_code.error'))
+  $('.zip_code').each(function () {
+    $(this).on("click", async () => {
+    let baseModal = {
+      title: I18n.t('zip_code.title'),
+      inputLabel: I18n.t('zip_code.description'),
+      input: 'number',
+      inputPlaceholder: '0000',
+      confirmButtonColor: '#007C50',
+      preConfirm: (zip_code) => {
+        if (!/^[1-9]\d{3}$/.test(zip_code)) {
+          Swal.showValidationMessage(I18n.t('zip_code.error'))
+        }
+        return zip_code;
       }
-      return zip_code;
     }
-  }
+    //add a "connect" button if the user in not connected
+    if (!$('#zip_code').data("signedin?")) {
+      const loginPath = $('#zip_code').data("login_path")
+      baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
+    }
 
-  if(!$('#zip_code').data("signedin?")) {
-    const loginPath = $('#zip_code').data("login_path")
-    baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
-  }
-  const { value: zip_code } = await Swal.fire(baseModal)
+    const {
+      value: zip_code
+    } = await Swal.fire(baseModal)
+    if(/^[1-9]\d{3}$/.test(zip_code)) {
+      const setZipCodePath = $('.zip_code').data("set_zip_code_path")
+      $.ajax({
+        data: {
+          authenticity_token: $('.zip_code').data("token"),
+          zip_code: zip_code
+        },
+        url: setZipCodePath,
+        type: "PATCH"
+      })
 
-  const setZipCodePath = $('#zip_code').data("set_zip_code_path")
-  $.ajax({
-    data: { authenticity_token: $('#zip_code').data("token"), zip_code:  zip_code},
-    url: setZipCodePath,
-    type: "PATCH"
+      $('.zip_code_number').each((_index, element) => {
+        $(element).text(zip_code)
+      })
+      $('#zip_code_input').val(zip_code)
+    }
+
+  })
   })
 
-  $('#zip_code_number').text(zip_code)
-})
 
+});
