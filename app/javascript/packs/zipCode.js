c3b71d0e36be9afb6d0b54b99114fbb6be3e6ba0
Remy Menard <remymenard.work@gmail.com>
2020-11-30 19:04:43 +0100

    add search input zip_code autofill when home_page loads

--- app/javascript/packs/zipCode.js
+++ app/javascript/packs/zipCode.js
@@ -2,50 +2,51 @@ import Swal from 'sweetalert2'
 
 document.addEventListener('turbolinks:load', () => {
 
+  if (/^[1-9]\d{3}$/.test($('.zip_code').text().trim())) {
+    $('#zip_code_input').val($('.zip_code').text().trim())
+  }
+
   $('.zip_code').each(function () {
     $(this).on("click", async () => {
-    let baseModal = {
-      title: I18n.t('zip_code.title'),
-      inputLabel: I18n.t('zip_code.description'),
-      input: 'number',
-      inputPlaceholder: '0000',
-      confirmButtonColor: '#007C50',
-      preConfirm: (zip_code) => {
-        if (!/^[1-9]\d{3}$/.test(zip_code)) {
-          Swal.showValidationMessage(I18n.t('zip_code.error'))
+      let baseModal = {
+        title: I18n.t('zip_code.title'),
+        inputLabel: I18n.t('zip_code.description'),
+        input: 'number',
+        inputPlaceholder: '0000',
+        confirmButtonColor: '#007C50',
+        preConfirm: (zip_code) => {
+          if (!/^[1-9]\d{3}$/.test(zip_code)) {
+            Swal.showValidationMessage(I18n.t('zip_code.error'))
+          }
+          return zip_code;
         }
-        return zip_code;
       }
-    }
-    const zipCodeInfos = $('.zip_code');
-    //add a "connect" button if the user in not connected
-    if (!zipCodeInfos.data("signedin?")) {
-      const loginPath = zipCodeInfos.data("login_path")
-      baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
-    }
+      const zipCodeInfos = $('.zip_code');
+      //add a "connect" button if the user in not connected
+      if (!zipCodeInfos.data("signedin?")) {
+        const loginPath = zipCodeInfos.data("login_path")
+        baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
+      }
 
-    const {
-      value: zip_code
-    } = await Swal.fire(baseModal)
-    if(/^[1-9]\d{3}$/.test(zip_code)) {
-      const setZipCodePath = zipCodeInfos.data("set_zip_code_path")
-      $.ajax({
-        data: {
-          authenticity_token: zipCodeInfos.data("token"),
-          zip_code: zip_code
-        },
-        url: setZipCodePath,
-        type: "PATCH"
-      })
+      const {
+        value: zip_code
+      } = await Swal.fire(baseModal)
+      if (/^[1-9]\d{3}$/.test(zip_code)) {
+        const setZipCodePath = zipCodeInfos.data("set_zip_code_path")
+        $.ajax({
+          data: {
+            authenticity_token: zipCodeInfos.data("token"),
+            zip_code: zip_code
+          },
+          url: setZipCodePath,
+          type: "PATCH"
+        })
 
-      $('.zip_code_number').each((_index, element) => {
-        $(element).text(zip_code)
-      })
-      $('#zip_code_input').val(zip_code)
-    }
-
+        $('.zip_code_number').each((_index, element) => {
+          $(element).text(zip_code)
+        })
+        $('#zip_code_input').val(zip_code)
+      }
+    })
   })
-  })
-
-
 });
