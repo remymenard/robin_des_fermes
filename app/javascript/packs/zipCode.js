1a7547257b6fb3383bef57a1bd7446f40e57f63b
Remy Menard <remymenard.work@gmail.com>
2020-12-01 12:16:52 +0100

    refacto zipCode JS script

--- app/javascript/packs/zipCode.js
+++ app/javascript/packs/zipCode.js
@@ -1,47 +1,67 @@
 import Swal from 'sweetalert2'
 
-document.addEventListener('turbolinks:load', () => {
-  $('.zip_code').each(function () {
-    $(this).on("click", async () => {
-      let baseModal = {
-        title: I18n.t('zip_code.title'),
-        inputLabel: I18n.t('zip_code.description'),
-        input: 'number',
-        inputPlaceholder: '0000',
-        confirmButtonColor: '#007C50',
-        preConfirm: (zip_code) => {
-          if (!/^[1-9]\d{3}$/.test(zip_code)) {
-            Swal.showValidationMessage(I18n.t('zip_code.error'))
-          }
-          return zip_code;
-        }
+document.addEventListener('turbolinks:load', async () => {
+  // variables
+  let baseModal = {
+    title: I18n.t('zip_code.title'),
+    inputLabel: I18n.t('zip_code.description'),
+    input: 'number',
+    inputPlaceholder: '0000',
+    confirmButtonColor: '#007C50',
+    preConfirm: (zip_code) => {
+      if (!/^[1-9]\d{3}$/.test(zip_code)) {
+        Swal.showValidationMessage(I18n.t('zip_code.error'))
       }
-      const zipCodeInfos = $('.zip_code');
-      //add a "connect" button if the user in not connected
-      if (!zipCodeInfos.data("signedin?")) {
-        const loginPath = zipCodeInfos.data("login_path")
-        baseModal.footer = `<a href="${loginPath}"> ${I18n.t('zip_code.login')} </a>`
-      }
+      return zip_code;
+    }
+  }
+  const zipCodeInfos = $('.zip_code');
+  const signedIn = zipCodeInfos.data("signedin?")
+  const loginPageHref = zipCodeInfos.data("login_path")
+  const ajaxRequestHref = zipCodeInfos.data("set_zip_code_path")
+  const authenticityToken = zipCodeInfos.data("token")
 
-      const {
-        value: zip_code
-      } = await Swal.fire(baseModal)
-      if (/^[1-9]\d{3}$/.test(zip_code)) {
-        const setZipCodePath = zipCodeInfos.data("set_zip_code_path")
-        $.ajax({
-          data: {
-            authenticity_token: zipCodeInfos.data("token"),
-            zip_code: zip_code
-          },
-          url: setZipCodePath,
-          type: "PATCH"
-        })
 
-        $('.zip_code_number').each((_index, element) => {
-          $(element).text(zip_code)
+  // methods
+  const findAllZipCodeButtonsOnPage = () => {
+    return $('.zip_code')
+  }
+
+  const addConnectButtonOnAlert = () => {
+    if (!signedIn) {
+      baseModal.footer = `<a href="${loginPageHref}"> ${I18n.t('zip_code.login')} </a>`
+    }
+  }
+
+  const applyClickableEventToButtons = (buttons) => {
+    buttons.each(function () {
+      $(this).on("click", () => {
+        Swal.fire(baseModal).then((zipCode) => {
+          launchAjaxRequest(zipCode.value);
         })
-        $('#zip_code_input').val(zip_code)
-      }
+      })
     })
-  })
+  }
+
+  const launchAjaxRequest = (zipCode) => {
+    if (/^[1-9]\d{3}$/.test(zipCode)) {
+      $.ajax({
+        data: {
+          authenticity_token: authenticityToken,
+          zip_code: zipCode
+        },
+        url: ajaxRequestHref,
+        type: "PATCH"
+      })
+
+      $('.zip_code_number').each((_index, element) => {
+        $(element).text(zipCode)
+      })
+      $('#zip_code_input').val(zipCode)
+    }
+  }
+  // startup logic
+  let buttons = findAllZipCodeButtonsOnPage();
+  addConnectButtonOnAlert();
+  applyClickableEventToButtons(buttons);
 });
