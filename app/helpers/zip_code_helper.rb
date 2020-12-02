774023a86b5332b4dc42df01080ab33385c57986
Remy Menard <remymenard.work@gmail.com>
2020-12-02 18:53:05 +0100

    remove get_zip_code_div method from helper and convert it to a partial

--- app/helpers/zip_code_helper.rb
+++ app/helpers/zip_code_helper.rb
@@ -4,15 +4,6 @@ module ZipCodeHelper
     !get_zip_code_number.nil?
   end
 
-  def get_zip_code_div
-    zip_code = get_zip_code_number
-    if zip_code
-      return "<p class='zip_code_number'>#{zip_code}</p>"
-    else
-      return "<p class='zip_code_number'>______</p>"
-    end
-  end
-
   def get_zip_code_number
     if user_signed_in?
       return current_user.zip_code if current_user.zip_code
