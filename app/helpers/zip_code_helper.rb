d939b85755f41b27cc1827bb14a85286d9316718
Remy Menard <remymenard.work@gmail.com>
2020-12-02 18:53:41 +0100

    rename zip_code? method to zip_code_set?

--- app/helpers/zip_code_helper.rb
+++ app/helpers/zip_code_helper.rb
@@ -1,16 +1,10 @@
 module ZipCodeHelper
 
-  def zip_code?
+  def zip_code_set?
     !get_zip_code_number.nil?
   end
 
   def get_zip_code_number
-    if user_signed_in?
-      return current_user.zip_code if current_user.zip_code
-      return nil
-    else
-      return cookies[:zip_code] if cookies[:zip_code]
-      return nil
-    end
+    user_signed_in? ? current_user.zip_code : cookies[:zip_code]
   end
 end
