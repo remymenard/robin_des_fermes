d01b115acea50290f2814bb82091c4fb928a0c36
Maelie Poussier <maelie.poussier@icloud.com>
2020-12-03 10:44:30 +0100

    just the migrations that have been relaunched

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,6 +15,13 @@ const initMapbox = () => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '80px';
+      element.style.height = '65px';
+
       new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
@@ -38,6 +45,7 @@ const initMapbox = () => {
         element.style.backgroundSize = 'contain';
         element.style.width = '80px';
         element.style.height = '65px';
+
 
         new mapboxgl.Marker(element)
           .setLngLat([ marker.lng, marker.lat ])
