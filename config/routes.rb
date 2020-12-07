303971bed9b245a246e7a0d14e3a3b7083c34fbd
Remy Menard <remymenard.work@gmail.com>
2020-12-07 13:58:09 +0100

    pass newsletter params to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /fr/ do
-    devise_for :users
+    devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
     root to: 'pages#home'
     resources :farms, only: [:index, :show]
     resources :products, only: [:show]
