cdc2fb93d13dffcce81762299db33e3de8991e6d
Remy Menard <remymenard.work@gmail.com>
2020-12-10 19:28:21 +0100

    deployment fix attempt #15

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://app-a38798ef-4d73-4bd1-b187-585f2c584033.cleverapps.io/" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :letter_opener
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
