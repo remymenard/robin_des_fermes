60ceba6af06e3e83860c9f49d86f9fcd10be9fd4
Nathan Menard <nathanmenard.work@gmail.com>
2020-12-11 10:38:11 +0100

    Add clever cloud staging URL
    WARNING : we'll have to put the proper url for staging and production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://https://app-a38798ef-4d73-4bd1-b187-585f2c584033.cleverapps.io" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
