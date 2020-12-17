59c7836a4f468bb25dc8c3ff722970447a30d35e
Maelie Poussier <maelie.poussier@icloud.com>
2020-12-17 09:30:35 +0100

    setup of the app on my new computer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_090701) do
+ActiveRecord::Schema.define(version: 2020_12_09_085646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
