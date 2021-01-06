20cc3c35434d7ab34a1311760c787405d75b605b
Remy Menard <remymenard.work@gmail.com>
2021-01-06 13:26:19 +0100

    remove seller id from orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_113429) do
+ActiveRecord::Schema.define(version: 2021_01_06_122455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,11 +86,9 @@ ActiveRecord::Schema.define(version: 2021_01_06_113429) do
     t.string "status", default: "waiting"
     t.string "transaction_id"
     t.bigint "buyer_id"
-    t.bigint "seller_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["buyer_id"], name: "index_orders_on_buyer_id"
-    t.index ["seller_id"], name: "index_orders_on_seller_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -145,7 +143,6 @@ ActiveRecord::Schema.define(version: 2021_01_06_113429) do
   add_foreign_key "farms", "users"
   add_foreign_key "opening_hours", "farms"
   add_foreign_key "orders", "users", column: "buyer_id"
-  add_foreign_key "orders", "users", column: "seller_id"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "farms"
 end
