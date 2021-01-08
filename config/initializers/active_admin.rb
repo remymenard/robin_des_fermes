f2dc1615b037bcbfa22616aac6a63150e129b714
Remy Menard <remymenard.work@gmail.com>
2021-01-08 11:35:24 +0100

    setup activeadmin for devise

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -64,6 +64,8 @@ ActiveAdmin.setup do |config|
   # CanCanAdapter or make your own. Please refer to documentation.
   # config.authorization_adapter = ActiveAdmin::CanCanAdapter
 
+  config.authorization_adapter = ActiveAdminAdapter
+
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
   # case when Pundit is unable to find suitable policy.
@@ -82,7 +84,7 @@ ActiveAdmin.setup do |config|
   # because, by default, user gets redirected to Dashboard. If user
   # doesn't have access to Dashboard, he'll end up in a redirect loop.
   # Method provided here should be defined in application_controller.rb.
-  # config.on_unauthorized_access = :access_denied
+  config.on_unauthorized_access = :access_denied
 
   # == Current User
   #
