3ecb66c6d851442561130b7e8179898ebb9c1125
Maelie Poussier <maelie.poussier@icloud.com>
2021-01-12 11:28:23 +0100

    we can add 2 photos to a farm

--- app/admin/farms/farms.rb
+++ app/admin/farms/farms.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Farm, as: "Exploitations" do
-  permit_params :name, :description, :photos, :address, :lagitude, :longitude, :opening_time, :labels, :country, :city, :iban, :zip_code, :farmer_number, :regions, :accepts_take_away, :user_id, :long_description, :delivery_delay, :accept_delivery
+  permit_params :name, :description, :address, :lagitude, :longitude, :opening_time, :labels, :country, :city, :iban, :zip_code, :farmer_number, :regions, :accepts_take_away, :user_id, :long_description, :delivery_delay, :accept_delivery, photos: []
   LABELS = ["Bio-Suisse", "IP-Suisse", "Suisse Garantie", "AOP", "IPG", "Naturabeef", "Demeter", "Bio-Suisse Reconversion"]
 
   form title: 'Exploitations' do |f|
@@ -50,6 +50,7 @@ ActiveAdmin.register Farm, as: "Exploitations" do
             input :delivery_delay, label: false
           end
           inputs 'Photos' do
+            input :photos, as: :file, input_html: { multiple: true }, label: false
             input :photos, as: :file, input_html: { multiple: true }, label: false
           end
         end
