bf6ffb91ffc92ed9806c3dc9f7335f29761c63d4
Maelie Poussier <maelie.poussier@icloud.com>
2021-02-10 15:06:13 +0100

    order index with only the order number

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Order, as: "Commandes"  do
+  permit_params :id, :price_cents, :price_currency, :status, :buyer_id
 
+  actions :all
+  index do
+    actions defaults: true
+    column 'Numéro de commande', :id
+  end
 end
