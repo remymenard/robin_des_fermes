3c11013e8561f314edfc5b9768d29f48ec607135
Maelie Poussier <maelie.poussier@icloud.com>
2021-02-16 14:39:03 +0100

    we can modify address of delivery

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,4 +2,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     stored_location_for(:user) || super
   end
+
+  def edit
+    skip_authorization
+    @user = current_user
+
+  end
+  def update
+    skip_authorization
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to stored_location_for(:user)
+    else
+      render 'edit'
+    end
+
+  end
+  private
+  def user_params
+    params.require(:user).permit(:address_line_1, :zip_code, :city)
+  end
 end
