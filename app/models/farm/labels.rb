ead311b8a21255c9956e572c2db2bb19602e95de
Remy Menard <remymenard.work@gmail.com>
2021-02-18 16:26:06 +0100

    hotfix 3 staging sprint 3

--- app/models/farm/labels.rb
+++ app/models/farm/labels.rb
@@ -1,3 +1,5 @@
-class Farm
-  LABELS = ["Bio-Suisse", "IP-Suisse", "Suisse Garantie", "AOP", "IPG", "Naturabeef", "Demeter", "Bio-Suisse Reconversion"]
+module Farm
+  class Farm
+    LABELS = ["Bio-Suisse", "IP-Suisse", "Suisse Garantie", "AOP", "IPG", "Naturabeef", "Demeter", "Bio-Suisse Reconversion"]
+  end
 end
