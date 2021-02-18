dc59181bbed265efe05de4054a100d50275991d4
Remy Menard <remymenard.work@gmail.com>
2021-02-18 11:34:35 +0100

    add preorder takeaway mails

--- app/jobs/send_order_confirmation_mails_job.rb
+++ app/jobs/send_order_confirmation_mails_job.rb
@@ -3,12 +3,17 @@ class SendOrderConfirmationMailsJob < ApplicationJob
 
   def perform(order)
     order.farm_orders.each do |farm_order|
+      mailer = OrderMailer.with({user: order.buyer, order: farm_order})
+
       if farm_order.status == 'waiting_shipping' && farm_order.takeaway_at_farm
-        OrderMailer.takeaway_confirmation_customer(order.buyer, farm_order).deliver
-        OrderMailer.takeaway_confirmation_owner(order.buyer, farm_order, farm_order.farm.user).deliver
+        mailer.takeaway_confirmation_customer.deliver_now
+        mailer.takeaway_confirmation_owner.deliver_now
       elsif farm_order.status == 'waiting_shipping' && (farm_order.standard_shipping || farm_order.express_shipping)
-        OrderMailer.delivery_confirmation_customer(order.buyer, farm_order).deliver
-        OrderMailer.delivery_confirmation_owner(order.buyer, farm_order, farm_order.farm.user).deliver
+        mailer.delivery_confirmation_customer.deliver_now
+        mailer.delivery_confirmation_owner.deliver_now
+      elsif farm_order.status == 'preordered' && farm_order.takeaway_at_farm
+        mailer.takeaway_preorder_customer.deliver_now
+        mailer.takeaway_preorder_owner.deliver_now
       end
     end
   end
