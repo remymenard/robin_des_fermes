e100d073a8c3aa7a6d64e4cd2c5654e95ea3cdd4
Remy Menard <remymenard.work@gmail.com>
2021-02-25 17:27:18 +0100

    skip authorization for search owner input controller

--- app/controllers/users/admins_controller.rb
+++ app/controllers/users/admins_controller.rb
@@ -1,9 +1,9 @@
 module Users
   class AdminsController < ApplicationController
     def search
+      skip_authorization
       @q = User.ransack(params[:q])
       render json: @q.result(distinct: true).map { |user| {id: user.id, full_name: user.first_name.capitalize + " " + user.last_name.capitalize} }
-      authorize current_user
     end
   end
 end
