68ccc67c3c152cd9d7f5cff794844f93a8fdf9ee
Remy Menard <remymenard.work@gmail.com>
2021-02-25 08:52:57 +0100

    fix order confirmation

--- app/javascript/packs/stopRecapOnFooter.js
+++ app/javascript/packs/stopRecapOnFooter.js
@@ -1,10 +1,3 @@
 export function stopRecapOnFooter() {
-  $(window).scroll(() => {
-    if($(window).width() < 992) return;
-    const footerToTop = $('.footer').position().top;
-    const scrollTop = $(document).scrollTop() + $(window).height();
-    const difference = scrollTop - footerToTop;
-    const bottomValue = scrollTop > footerToTop ? difference : 0;
-    $('.position-relative').css('bottom', bottomValue/2);
-});
+
   }
