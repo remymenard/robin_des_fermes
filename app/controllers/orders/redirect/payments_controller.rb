e60cdda87bf78a02616ddfb171abd5438a8be6f2
Remy Menard <remymenard.work@gmail.com>
2021-03-08 00:43:02 +0100

    catch error for mails

--- app/controllers/orders/redirect/payments_controller.rb
+++ app/controllers/orders/redirect/payments_controller.rb
@@ -12,15 +12,21 @@ module Orders
             order.farm_orders.each do |farm_order|
               if farm_order.contains_preorder_product?
                 farm_order.update(price: farm_order.total_price_with_shipping, status: 'preordered', waiting_for_preorder_at: Date.current, waiting_for_shipping_at: farm_order.compute_preorder_delivery_date)
-                SendOrderReminderMailsJob.set(wait_until: farm_order.preorder_shipping_starting_at - 7.days).perform_later(farm_order)
+                begin
+                  SendOrderReminderMailsJob.set(wait_until: farm_order.preorder_shipping_starting_at - 7.days).perform_later(farm_order)
+                end
               else
                 farm_order.update(price: farm_order.total_price_with_shipping, status: 'in_preparation', waiting_for_shipping_at: Date.current)
-                SendOrderReminderMailsJob.set(wait_until: farm_order.waiting_for_shipping_at + farm_order.farm.delivery_delay.days - 1.day).perform_later(farm_order)
+                begin
+                  SendOrderReminderMailsJob.set(wait_until: farm_order.waiting_for_shipping_at + farm_order.farm.delivery_delay.days - 1.day).perform_later(farm_order)
+                end
               end
             end
           end
         end
-        SendOrderConfirmationMailsJob.perform_now(order)
+        begin
+          SendOrderConfirmationMailsJob.perform_now(order)
+        end
         @reassurance = true
       end
 
