0a720331ef961e55db02de28639a55dac9ead163
Remy Menard <remymenard.work@gmail.com>
2021-03-08 01:06:13 +0100

    add method handling in application mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,6 @@
 class OrderMailer < ApplicationMailer
   include Rails.application.routes.url_helpers
 
-  rescue_from Postmark::InactiveRecipientError, with: :reactivate_and_retry
-
   before_action :define_order_and_user
 
   default from: ENV["DEFAULT_EMAIL"]
@@ -65,24 +63,5 @@ class OrderMailer < ApplicationMailer
     @order = params[:order]
     @user = params[:user]
     @owner = @order.farm.user
-  end
-
-  def postmark_client
-    ::Postmark::ApiClient.new(ActionMailer::Base.postmark_settings[:api_token],
-                              ActionMailer::Base.postmark_settings.except(:api_token))
-  end
-
-  def reactivate_and_retry(error)
-    Rails.logger.info("Error when sending #{message} to #{error.recipients.join(', ')}")
-    Rails.logger.info(error)
-
-    error.recipients.each do |recipient|
-      bounce = postmark_client.bounces(emailFilter: recipient).first
-      next unless bounce
-      postmark_client.activate_bounce(bounce[:id])
-    end
-
-    # Try again immediately
-    message.deliver
   end
 end
