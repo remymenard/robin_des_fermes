11e5897b4c991a3cf21451ea88d7f99e18b26d5e
Remy Menard <remymenard.work@gmail.com>
2021-09-13 14:07:05 +0200

    fix postmark error when an email doesn't exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_policy_scoped, only: :index, unless: [:skip_pundit?, :active_admin_controller?]
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Postmark::InactiveRecipientError, with: :reactivate_and_retry
 
   http_basic_authenticate_with name: "rdf_admin", password: "rdf_rdf_2020" if Rails.env.staging?
 
@@ -73,5 +74,24 @@ class ApplicationController < ActionController::Base
     else
       session[:mixpanel_id] = SecureRandom.uuid if session[:mixpanel_id].nil?
     end
+  end
+
+  def postmark_client
+    ::Postmark::ApiClient.new(ActionMailer::Base.postmark_settings[:api_token],
+                              ActionMailer::Base.postmark_settings.except(:api_token))
+  end
+
+  def reactivate_and_retry(error)
+    Rails.logger.info("Error when sending #{message} to #{error.recipients.join(', ')}")
+    Rails.logger.info(error)
+
+    error.recipients.each do |recipient|
+      bounce = postmark_client.bounces(emailFilter: recipient).first
+      next unless bounce
+      postmark_client.activate_bounce(bounce[:id])
+    end
+
+    # Try again immediately
+    message.deliver
   end
 end
