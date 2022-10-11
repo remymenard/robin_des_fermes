edab8bd9c8ef04e3f8a420e8e9f8fafdd9bdd0ae
eth-ivan <ivan@perroud.me>
2022-10-11 14:12:14 +0200

    refactor javascript

--- app/javascript/packs/addToBasketFromIndex.js
+++ app/javascript/packs/addToBasketFromIndex.js
@@ -21,22 +21,13 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// this function gets the counter (nb products to add to basket)
-function getNbProducts(childElement) {
-  const parentElement = $(childElement).parents()[2];
-  const str = $(parentElement).find(".cart-counter")[0].innerText;
-  return parseInt(str);
-};
-
-
 function sendAjaxRequest(e, nbProducts) {
   e.preventDefault();
   $(e.target).LoadingOverlay("show", { imageColor: "#339E72" });
 
-  // route replacement with nb products to add to basket
-  const default_suffix = 'increment/1';
-  const new_suffix = `increment/${nbProducts}`;
-  const hrefPath = $(e.target).data("path").replace(default_suffix, new_suffix);
+  // route replacement with correct amount of products to add to basket
+  // /basket/order_line_items/product_id/increment/1 ==> /basket/order_line_items/product_id/increment/nbProducts
+  const hrefPath = $(e.target).data("path").replace('increment/1', `increment/${nbProducts}`);
   const token = $(e.target).data("token");
  
   $.ajax({
@@ -46,13 +37,9 @@ function sendAjaxRequest(e, nbProducts) {
     url: hrefPath,
     type: "POST",
     success: (answer) => {
-      $("#popupcontent").html(answer);
-      $("#overlay").addClass('show');
-      $("#popup").addClass('show');
       $("#basket").html(answer);
       updateNavbarInfos();
       sendAjaxRequestModal(e, nbProducts)
-      $(e.target).LoadingOverlay("hide");
     },
     error: () => {
       $(e.target).LoadingOverlay("hide");
@@ -60,16 +47,12 @@ function sendAjaxRequest(e, nbProducts) {
   })
 };
 
-// basket modal, route replacement :
-// /basket/order_line_items/product_id/increment/1 replaced by
-// /basket/order_line_items/product_id/basket_modal/nbProducts
 function sendAjaxRequestModal(e, nbProducts) {
   e.preventDefault();
 
-  // get correct route (see above)
-  const default_suffix = 'increment/1';
-  const new_suffix = `basket_modal/${nbProducts}`;
-  const hrefPath = $(e.target).data("path").replace(default_suffix, new_suffix);
+  // route replacement : the view _basket_modal_popup.html.erb has another route
+  // /basket/order_line_items/product_id/increment/1 ==> /basket/order_line_items/product_id/basket_modal/nbProducts
+  const hrefPath = $(e.target).data("path").replace('increment/1', `basket_modal/${nbProducts}`);
   const token = $(e.target).data("token");
 
   $.ajax({
@@ -82,9 +65,17 @@ function sendAjaxRequestModal(e, nbProducts) {
       $("#popupcontent").html(answer);
       $("#overlay").addClass('show');
       $("#popup").addClass('show');
+      $(e.target).LoadingOverlay("hide");
     },
     error: () => {
-      console.log("error");
+      location.reload();
     }
   })
+};
+
+// this function gets the counter (nb products to add to basket)
+function getNbProducts(childElement) {
+  const parentElement = $(childElement).parents()[2];
+  const str = $(parentElement).find(".cart-counter")[0].innerText;
+  return parseInt(str);
 };
